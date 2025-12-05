cba55d174e43a37fcec3e917a59412ff6891cdcf
raheja2709 <jr397051@gmail.com>
2025-12-05 23:14:49 +0530

    Initial commit with project files

---
+++ src/main/java/com/devops/spring/DevopsSpringBootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.devops.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsSpringBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsSpringBootDemoApplication.class, args);
+	}
+
+}
